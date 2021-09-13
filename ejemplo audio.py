42c46474caa008479e614dbbbb804b097f2dc2a7
TheRadDani <ldanielfch@gmail.com>
2021-09-13 13:39:07 -0600

    m

--- ejemplo audio.py
+++ ejemplo audio.py
@@ -1,4 +1,10 @@
-#ejemplo procesar audio
+#import numpy as np
+#from numpy.fft import fft,ifft
+#import matplotlib.pyplot as plt #biblioteca para graficar
+import scipy.io.wavfile as waves #biblioteca para procesar audio .wav en python
+import scipy.integrate as integrate
+from scipy import signal
+ #ejemplo procesar audio
 import numpy as np
 from numpy.fft import fft,ifft
 import matplotlib.pyplot as plt #biblioteca para graficar
@@ -18,42 +24,55 @@ muestreo, sonido = waves.read(archivo) #leer archivo de audio, devuelve la frecu
 tamano_sonido = len(sonido.shape) #tamano de todas las muestras
 dt = 1/muestreo #Periodo de muestreo 
 t = np.arange(0,tamano_sonido*dt,dt) #valor de intervalos de tiempo del audio
-ruido=2000*np.random.random(len(t))#senal de ruido blanco
 #procesamiento de canales del audio
 #disenar para que selecciones canal monofonico o estereo, seguan sea el caso
 canal = 0 #monofonico 
+#ruido=100*np.random.random(122868)#senal de ruido blanco
 if (tamano_sonido==1): # Monofónico
-    uncanal=sonido[:]  
+    uncanal=sonido[:] #+ruido
 if (tamano_sonido==2): # Estéreo
-    uncanal=sonido[:,canal]
+    uncanal=sonido[:,canal] #+ruido
+
 #uncanal = sonido[:] #canal monofonico (todas las muestars)
-'''
-procedimiento de codificacion delta-sigma
-'''
-senal_real=sonido+ruido #senal de sonido con ruido blanco
+
+wn= 22050/30000
+b, a = signal.butter(8, wn, 'lowpass',analog=True)   #Configuration filter 8 representa el orden del filtro
+filtedData = signal.filtfilt(b, a, uncanal)  #data es la señal a filtrar
+X = fft(filtedData)
+N = len(X)
+n = np.arange(N)
+T = N*2.26e-05
+freq = n/T - 22050
+
+
+
+#procedimiento de codificacion delta-sigma
+
+ #senal de sonido con ruido blanco
 ganancia_deltaY=0.3
-deltaY = ganancia_deltaY*np.max(senal_real) #senal deltaY ganancia*maximo de senal real
-deltaT = dt #periodo de senal delta
+deltaY = ganancia_deltaY*np.max(sonido) #senal deltaY ganancia*maximo de senal real
+#deltaT = dt #periodo de senal delta
+deltaT = 2.2e-06
 i_muestras = int(0/deltaT) #intervalo muestras
-tf_muestra=0.1 #hasta aqui llega el muestreo
+tf_muestra=10*0.002 #hasta aqui llega el muestreo
 t_muestra=np.arange(0,tf_muestra,deltaT) #tiempo que dura senal de muestra del audio
-k = len(t_muestra) #
-muestra= np.copy(uncanal[:i_muestras+k])#solo una muestra de la sanal del canal monofonico
+tamano = len(t_muestra) #
+muestra= np.copy(uncanal[:i_muestras+tamano])#solo una muestra de la sanal del canal monofonico
 
 # Señal Digital
-vT = np.zeros(k, dtype=float)  #vector de valores codificados de la senal real  
-bkT = np.zeros(k, dtype=int) #vector de bits a la salida de la senal codificada
+vT = np.zeros(tamano, dtype=float)  #vector de valores codificados de la senal real  
+bkT = np.zeros(tamano, dtype=int) #vector de bits a la salida de la senal codificada
 
-for i in range(1,k):
+for k in range(1,tamano):
     #pwm
-    diferencia = muestra[i]-vT[i-1]
-    if (diferencia>0):
+    dif = muestra[k]-vT[k-1]
+    if (dif>0):
         bit = 1
     else:
         bit = -1
-    vT[i] = vT[i-1]+bit*deltaY
-    bkT[i] = bit
-parametros=np.array([deltaT,deltaY,k])
+    vT[k] = vT[k-1]+bit*deltaY
+    bkT[k] = bit
+parametros=np.array([deltaT,deltaY,tamano])
 
 # SALIDA
 print('Parametros: ')
@@ -71,6 +90,23 @@ cuadrado = muestra**2
 energia = integrate.simps(cuadrado,t_muestra)
 # SALIDA
 print(f" Energia de la onda del audio (muestra) : {energia:.2f}")
+
+'''
+plt.figure() 
+# Senal real en el dominio del tiempo
+plt.figure(figsize = (12, 6))
+plt.suptitle('Senal real en el dominio del tiempo')
+plt.subplot(211)    # grafica de 2x1 y subgrafica 1
+plt.ylabel('Senal real (t)')
+plt.xlabel('t (s)')
+plt.plot(t,sonido[:,0], 'r')
+'''
+plt.figure() 
+plt.figure(figsize = (12, 6))
+plt.subplot(211)
+plt.xlabel('Freq (Hz)')
+plt.ylabel('FFT Amplitude |X(freq)|')
+plt.plot(freq,np.abs(X))
 
 
 plt.figure() 
@@ -109,7 +145,10 @@ plt.show()
 '''
 Decodificacion
 '''
+#leer datos
 
+bkT_info=np.loadtxt('deltasigma_datos.txt',dtype=int)
+datos=np.loadtxt('deltasigma_parametros.txt',dtype=float)
 
 
 
